2f41e9afdc9ed0fb714ec3c78c0388ae132ae43f
chlqudco <chlqudco2@naver.com>
2022-08-08 22:55:44 +0900

    (Firebase) email 회원가입 로그인 구현 완료

--- app/src/main/java/com/chlqudco/develop/loginsummary/EmailLogInActivity.kt
+++ app/src/main/java/com/chlqudco/develop/loginsummary/EmailLogInActivity.kt
@@ -2,10 +2,81 @@ package com.chlqudco.develop.loginsummary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.chlqudco.develop.loginsummary.databinding.ActivityEmailLogInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class EmailLogInActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityEmailLogInBinding.inflate(layoutInflater) }
+    private val auth: FirebaseAuth by lazy { Firebase.auth }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_email_log_in)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        //로그인결과 텍스트 초기화
+        if (auth.currentUser != null){
+            successLogIn()
+        } else{
+            failLogIn()
+        }
+
+        //로그인 버튼 클릭
+        binding.EmailLogInLogInButton.setOnClickListener {
+            //예외처리 따위 안함
+            val email = binding.EmailLogInIdEditText.text.toString()
+            val password = binding.EmailLogInPasswordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){task ->
+                    if (task.isSuccessful){
+                        successLogIn()
+                    } else{
+                        failLogIn()
+                    }
+                }
+        }
+
+        //로그아우 버튼 클릭
+        binding.EmailLogInLogOutButton.setOnClickListener {
+            auth.signOut()
+            failLogIn()
+        }
+
+        //회원가입 버튼 클릭
+        binding.EmailLogInSignUpButton.setOnClickListener {
+            //예외처리 따위 안함
+            val email = binding.EmailLogInIdEditText.text.toString()
+            val password = binding.EmailLogInPasswordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인 버튼을 눌러주세요.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "회원가입에 실패했습니다. 이미 가입한 이메일일 수 있습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+    }
+
+    private fun failLogIn() {
+        binding.EmailLogInResultTextView.text = "로그인 안됨"
+        binding.EmailLogInLogInButton.isEnabled = true
+        binding.EmailLogInLogOutButton.isEnabled = false
+    }
+
+    private fun successLogIn() {
+        binding.EmailLogInResultTextView.text = "로그인 됨"
+        binding.EmailLogInLogInButton.isEnabled = false
+        binding.EmailLogInLogOutButton.isEnabled = true
     }
 }
--- app/src/main/java/com/chlqudco/develop/loginsummary/MainActivity.kt
+++ app/src/main/java/com/chlqudco/develop/loginsummary/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.chlqudco.develop.loginsummary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,23 @@ class MainActivity : AppCompatActivity() {
 
         //다크모드 금지
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    fun buttonClicked(v: View) {
+
+        val intent: Intent = when(v.id){
+            R.id.appleLogInButton ->  Intent(this, AppleLogInActivity::class.java)
+            R.id.emailLogInButton ->  Intent(this, EmailLogInActivity::class.java)
+            R.id.faceBookLogInButton ->  Intent(this, FaceBookLogInActivity::class.java)
+            R.id.gitHubLogInButton ->  Intent(this, GitHubLogInActivity::class.java)
+            R.id.googleLogInButton ->  Intent(this, GoogleLogInActivity::class.java)
+            R.id.guestLogInButton -> Intent(this, GuestLogInActivity::class.java)
+            R.id.kakaoLogInButton ->  Intent(this, KakaoLogInActivity::class.java)
+            R.id.microsoftLogInButton ->  Intent(this, MicrosoftLogInActivity::class.java)
+            R.id.naverLogInButton ->  Intent(this, NaverLogInActivity::class.java)
+            R.id.phoneLogInButton ->  Intent(this, PhoneLogInActivity::class.java)
+            else ->  Intent(this, AppleLogInActivity::class.java)
+        }
+        startActivity(intent)
     }
 }
