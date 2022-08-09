3355ad59779eb3e9cc6b56165bceaef076d7a5d3
chlqudco <chlqudco2@naver.com>
2022-08-09 15:39:52 +0900

    (Firebase) 익명 로그인 구현 완료

--- app/src/main/java/com/chlqudco/develop/loginsummary/GoogleLogInActivity.kt
+++ app/src/main/java/com/chlqudco/develop/loginsummary/GoogleLogInActivity.kt
@@ -1,27 +1,19 @@
 package com.chlqudco.develop.loginsummary
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.chlqudco.develop.loginsummary.databinding.ActivityGoogleLogInBinding
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import java.lang.Exception
 
 class GoogleLogInActivity : AppCompatActivity() {
 
--- app/src/main/java/com/chlqudco/develop/loginsummary/GuestLogInActivity.kt
+++ app/src/main/java/com/chlqudco/develop/loginsummary/GuestLogInActivity.kt
@@ -1,11 +1,64 @@
 package com.chlqudco.develop.loginsummary
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.chlqudco.develop.loginsummary.databinding.ActivityGuestLogInBinding
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class GuestLogInActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityGuestLogInBinding.inflate(layoutInflater) }
+    private val auth by lazy { Firebase.auth }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_log_in)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        if (auth.currentUser != null){ auth.signOut() }
+        initLogIn()
+
+        //로그인 처리
+        binding.GuestLogInButton.setOnClickListener {
+            logIn()
+        }
+
+        //로그아웃 처리
+        binding.GuestLogOutButton.setOnClickListener {
+            auth.signOut()
+            initLogIn()
+        }
+    }
+
+    private fun logIn() {
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    successLogIn()
+                } else {
+                    initLogIn()
+                }
+            }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun successLogIn() {
+        binding.GuestLogInButton.isEnabled = false
+        binding.GuestLogOutButton.isEnabled = true
+        binding.GuestResultTextView.text = "로그인 성공"
+    }
+
+    private fun initLogIn() {
+        binding.GuestLogInButton.isEnabled = true
+        binding.GuestLogOutButton.isEnabled = false
+        binding.GuestResultTextView.text = "로그인 안됨"
     }
 }
