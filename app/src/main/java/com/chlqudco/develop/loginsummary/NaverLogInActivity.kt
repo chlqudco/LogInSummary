31ac646904c5d1673817248133f15df65f578fc5
chlqudco <chlqudco2@naver.com>
2022-08-10 15:01:28 +0900

    Naver 로그인 구현 완료

--- app/src/main/java/com/chlqudco/develop/loginsummary/NaverLogInActivity.kt
+++ app/src/main/java/com/chlqudco/develop/loginsummary/NaverLogInActivity.kt
@@ -2,10 +2,64 @@ package com.chlqudco.develop.loginsummary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chlqudco.develop.loginsummary.databinding.ActivityNaverLogInBinding
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 
 class NaverLogInActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityNaverLogInBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_naver_log_in)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        NaverIdLoginSDK.initialize(this, "gCRYXhmVbGVAVWTBGPZu", "yrokdxv8Uh", "LogInSummary")
+
+        //로그아웃 하고 시작
+        NaverIdLoginSDK.logout()
+
+        //로그인 버튼
+        binding.NaverLogInButton.setOnClickListener {
+            logIn()
+        }
+
+        //로그아웃 버튼
+        binding.NaverLogOutButton.setOnClickListener {
+            logOut()
+        }
+    }
+
+    private fun logIn() {
+        var token: String? =""
+
+        val profileCallback = object : NidProfileCallback<NidProfileResponse>{
+            override fun onSuccess(result: NidProfileResponse) { binding.NaverResultTextView.text = "로그인 성공" }
+            override fun onFailure(httpStatus: Int, message: String) { binding.NaverResultTextView.text = "로그인 실패" }
+            override fun onError(errorCode: Int, message: String) { binding.NaverResultTextView.text = "로그인 오류" }
+        }
+
+        val oauthLoginCallback = object : OAuthLoginCallback{
+            override fun onSuccess() {
+                token = NaverIdLoginSDK.getAccessToken()
+                NidOAuthLogin().callProfileApi(profileCallback)
+            }
+            override fun onFailure(httpStatus: Int, message: String) { binding.NaverResultTextView.text = "로그인 실패" }
+            override fun onError(errorCode: Int, message: String) { binding.NaverResultTextView.text = "로그인 오류" }
+        }
+
+        NaverIdLoginSDK.authenticate(this, oauthLoginCallback)
+    }
+
+    private fun logOut() {
+        NaverIdLoginSDK.logout()
+        binding.NaverResultTextView.text = "로그인 안함"
     }
 }
